a5a06d2e4190f944b322d06ef5c3a37cad9eaa42
raj.das <rajdas2412@gmail.com>
2023-03-04 23:04:21 -0500

    Lambda Pipeline

--- democdkpipe/democdkpipe_stack.py
+++ democdkpipe/democdkpipe_stack.py
@@ -29,15 +29,15 @@ class DemocdkpipeStack(Stack):
                                                       )
                                       )
 
-        # test_stage = demo_cicd_pipe.add_stage(PipelineAppStage(self,
-        #                                                        "TEST",
-        #                                                        env=cdk.Environment(account="684119280118",
-        #                                                                            region="us-east-1")
-        #                                                        ))
-        # test_stage.add_post(ManualApprovalStep("Approval"))
-        #
-        # prod_stage = demo_cicd_pipe.add_stage(PipelineAppStage(self,
-        #                                                        "PROD",
-        #                                                        env=cdk.Environment(account="684119280118",
-        #                                                                            region="us-east-1")
-        #                                                        ))
+        test_stage = demo_cicd_pipe.add_stage(PipelineAppStage(self,
+                                                               "TEST",
+                                                               env=cdk.Environment(account="684119280118",
+                                                                                   region="us-east-1")
+                                                               ))
+        test_stage.add_post(ManualApprovalStep("Approval"))
+
+        prod_stage = demo_cicd_pipe.add_stage(PipelineAppStage(self,
+                                                               "PROD",
+                                                               env=cdk.Environment(account="684119280118",
+                                                                                   region="us-east-1")
+                                                               ))
